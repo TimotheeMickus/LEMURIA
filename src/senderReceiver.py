bb7a77839060e982f0c64d5ea5864c2807c3f919
airc <timothee.mickus@univ-lorraine.fr>
2020-02-18 18:44:38 +0900

    loading issue in sender-receiver

--- src/senderReceiver.py
+++ src/senderReceiver.py
@@ -3,17 +3,23 @@ import torch.nn as nn
 
 from sender import Sender
 from receiver import Receiver
-from modules import build_cnn_encoder_from_args
+from modules import build_cnn_encoder_from_args, build_embeddings
 
 class SenderReceiver(nn.Module):
-    def __init__(self, image_encoder, symbol_embeddings):
+    def __init__(self, sender, receiver, check_shared_params=True):
         nn.Module.__init__(self)
 
-        self.sender = Sender(image_encoder, symbol_embeddings)
-        self.receiver = Receiver(image_encoder, symbol_embeddings)
+        if check_shared_params:
+            assert sender.image_encoder is receiver.image_encoder, 'parameters are not shared'
+            assert receiver.message_encoder.symbol_embeddings is sender.message_decoder.symbol_embeddings, 'parameters are not shared'
+
+        self.sender = sender
+        self.receiver = receiver
 
     @classmethod
     def from_args(cls, args):
         image_encoder = build_cnn_encoder_from_args(args)
-        symbol_embeddings = build_embeddings(args.hidden_size, use_bos=True) # +2: padding symbol, BOS symbol
-        return cls(image_encoder, symbol_embeddings)
+        symbol_embeddings = build_embeddings(args.base_alphabet_size, args.hidden_size, use_bos=True) # +2: padding symbol, BOS symbol
+        sender = Sender.from_args(args, image_encoder=image_encoder, symbol_embeddings=symbol_embeddings)
+        receiver = Receiver.from_args(args, image_encoder=image_encoder, symbol_embeddings=symbol_embeddings)
+        return cls(sender, receiver)
