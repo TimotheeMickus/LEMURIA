cbd532ebb5093d66e38fa2f8e5f4a8c552642434
Timothee Mickus <timothee.mickus@univ-lorraine.fr>
2020-02-20 22:23:52 +0900

    baselines for compositionality metric(s)

--- cbc/eval/lang_metric.py
+++ cbc/eval/lang_metric.py
@@ -8,7 +8,7 @@ from scipy.stats import spearmanr as spearman
 def read_csv(csv_filename):
 
     with open(csv_filename) as istr:
-        data = csv.reader(data, delimiter="\t")
+        data = list(csv.reader(istr, delimiter="\t"))
 
     _, categories, messages = zip(*data)
 
@@ -18,20 +18,23 @@ def read_csv(csv_filename):
 
     categories = map(str.strip, categories)
     categories = map(str.split, categories)
-    categories = [list(map(int, ctg)) for ctg in categories]
+    categories = [list(map(eval, ctg)) for ctg in categories]
 
     return messages, categories
 
 def compute_correlation(messages, categories):
 
     messages = [''.join(map(chr, msg)) for msg in messages]
-    categories = [''.join(map(int, ctg)) for ctg in categories]
+    categories = [''.join(map(chr, map(int, ctg))) for ctg in categories]
 
     messages = list(it.starmap(levenshtein, it.combinations(messages, 2)))
     categories = list(it.starmap(hamming, it.combinations(categories, 2)))
 
-    return spearman(categories, messssages)
+    return spearman(categories, messages)
 
 def main(args):
+    assert args.messages_file is not None, "Messages are required."
+
     messages, categories = read_csv(args.messages_file)
+
     print(compute_correlation(messages, categories))
---
+++ scripts/langrefs-gen/make_reference_languages.py
@@ -0,0 +1,18 @@
+import random
+import itertools as it
+
+with open("regular.tsv", "w") as ostr:
+    for id, category in enumerate(it.product([True, False], repeat=5)):
+            message = [2 * i + int(c) for i,c in enumerate(category)] + [10]
+            print(id, ' '.join(map(str, category)), ' '.join(map(str, message)), sep="\t", file=ostr)
+
+rolls = 3
+with open("random-order.tsv", "w") as ostr:
+    idx = 0
+    for i in range(rolls):
+        for category in it.product([True, False], repeat=5):
+            message = [2 * i + int(c) for i,c in enumerate(category)]
+            random.shuffle(message)
+            message += [10]
+            print(idx, ' '.join(map(str, category)), ' '.join(map(str, message)), sep="\t", file=ostr)
+            idx += 1
