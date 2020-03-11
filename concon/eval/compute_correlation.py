e1e4e2b06d1416f70b04ec1739ffce252422c2b8
Timothée Bernard <tot.c.moi@gmail.com>
2020-03-11 19:30:08 +0900

    new lev-based distance!

--- concon/eval/compute_correlation.py
+++ concon/eval/compute_correlation.py
@@ -36,12 +36,12 @@ def read_csv(csv_filename, string_msgs=False):
 
     return messages, categories
 
-# `str1` and `str2` can be any sequences
+# `seq1` and `seq2` can be any sequences
 # `embeddings` must currently have a get method for element of the sequences to Numpy arrays (or None)
 # `average_distance` and `r` are two hyperparameters: substituting two items the embeddings of which are at distance `average_distance` (in L2) will have a cost `r`. For smaller (resp. higher) distance, the cost of the substitution will be smaller (resp. higher), but, of course, always between 0 and 1.
-def word_embedding_levenshtein(str1, str2, embeddings, average_distance, r=0.9, normalise=False):
-    x1 = 1 + len(str1)
-    x2 = 1 + len(str2)
+def word_embedding_levenshtein(seq1, seq2, embeddings, average_distance, r=0.9, normalise=False):
+    x1 = 1 + len(seq1)
+    x2 = 1 + len(seq2)
 
     alpha = r / ((1 - r) * average_distance)
 
@@ -54,30 +54,73 @@ def word_embedding_levenshtein(str1, str2, embeddings, average_distance, r=0.9, 
     # Core of the algorithm
     for i in range(1, x1):
         for j in range(1, x2):
-            wa = str1[i-1]
-            wb = str2[j-1]
+            e1 = seq1[i-1]
+            e2 = seq2[j-1]
 
-            if(wa == wb): c = 0
+            if(e1 == e2): c = 0
             else:
-                ea = embeddings.get(wa)
-                eb = embeddings.get(wb)
+                v1 = embeddings.get(e1)
+                v2 = embeddings.get(e2)
 
-                if((ea is None) or (eb is None)): c = 1
+                if((v1 is None) or (v2 is None)): c = 1
                 else:
-                    dst = np.linalg.norm(ea - eb) # Distance 2 (or L2 norm of the difference)
+                    dst = np.linalg.norm(v1 - v2) # Distance 2 (or L2 norm of the difference)
 
                     # Now, we need a function increasing function mapping 0 to 0 and +inf to 1
                     c = 1 - (1 / (1 + (alpha * dst)))
 
+                    #c /= r # If you uncomment this line, the cost of a substitution at distance `average_distance` will be 1 and substitutions might have higher cost, up to 1/r. This might be justified as long as `r` is above 0.5 (otherwise, some substitutions might be more expensive than an insertion followed by a deletion).
+
             d[i][j] = min(
-                (d[(i-1)][j] + 1), # Deletion of str1[i]
-                (d[i][(j-1)] + 1), # Insertion of str2[j]
-                (d[(i-1)][(j-1)] + c)) # Substitution from str1[i] to str2[j]
+                (d[(i-1)][j] + 1), # Deletion of seq1[i]
+                (d[i][(j-1)] + 1), # Insertion of seq2[j]
+                (d[(i-1)][(j-1)] + c)) # Substitution from seq1[i] to seq2[j]
             )
 
     raw = d[-1][-1]
 
-    if(normalise): return (raw / (len(str1) + len(str2)))
+    if(normalise): return (raw / (len(seq1) + len(seq2)))
+    return raw
+
+# `weights` is a dictionary of word to weight (between 0 and 1)
+# One possibility for computing these weights: for a given token, let f be its document frequency (the proportion of documents in which it appears), then w = 1 - f
+def weighted_levenshtein(seq1, seq2, weights, normalise=False):
+    x1 = 1 + len(seq1)
+    x2 = 1 + len(seq2)
+
+    alpha = r / ((1 - r) * average_distance)
+
+    # Initialisation of the matrix
+    d = [] # Using Numpy structures for this is probably not more efficient
+    tmp = 0.0
+    first_line = [tmp]
+    for e in seq2:
+        tmp += weights.get(e, 1)
+        first_line.append(tmp)
+    d.append(first_line)
+    tmp = 0
+    for e in seq1:
+        tmp += weights.get(e, 1)
+        d.append([tmp] * x2)
+
+    # Core of the algorithm
+    for i in range(1, x1):
+        for j in range(1, x2):
+            e1 = seq1[i-1]
+            e2 = seq2[j-1]
+
+            w1 = weights.get(e1, 1)
+            w2 = weights.get(e2, 1)
+
+            d[i][j] = min(
+                (d[(i-1)][j] + w1), # Deletion of seq1[i]
+                (d[i][(j-1)] + w2), # Insertion of seq2[j]
+                (d[(i-1)][(j-1)] + (int(e1 != e2) * max(w1, w2)))) # Substitution from seq1[i] to seq2[j]
+            )
+
+    raw = d[-1][-1]
+
+    if(normalise): return (raw / (r[0][-1] + r[-1][0]))
     return raw
 
 @ft.lru_cache(maxsize=32768)
