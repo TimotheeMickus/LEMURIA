0df27eceea89ece427f309f79442167e3129543a
Timothee Mickus <timothee.mickus@univ-lorraine.fr>
2022-02-08 21:26:46 +0100

    eval charlie

--- concon/games/aliceBobCharlie.py
+++ concon/games/aliceBobCharlie.py
@@ -10,7 +10,7 @@ import tqdm
 
 from .game import Game
 from ..agents import Sender, Receiver, Drawer
-from ..utils.misc import show_imgs, max_normalize_, to_color, build_optimizer, pointing, compute_entropy_stats
+from ..utils.misc import show_imgs, max_normalize_, to_color, build_optimizer, pointing, compute_entropy_stats, pointing
 from ..eval import compute_correlation
 
 from .aliceBob import AliceBob
@@ -150,3 +150,159 @@ class AliceBobCharlie(AliceBob):
             action = sender(self._alice_input(batch)).action
             images = drawer(*action).image
         return torch.cat([batch.target_img().unsqueeze(1), images.unsqueeze(1)], dim=1)
+
+    def evaluate(self, data_iterator, epoch):
+        print('using charlie specific eval loop')
+        training_state = self.train_charlie
+        self.switch_charlie(False) # also controls image ordering
+        def log(name, value):
+            self.autologger._write(name, value, epoch, direct=True)
+            if(self.autologger.display != 'minimal'): print('%s\t%s' % (name, value))
+
+        counts_matrix = np.zeros((data_iterator.nb_categories, data_iterator.nb_categories))
+        failure_matrix = np.zeros((data_iterator.nb_categories, data_iterator.nb_categories))
+
+        # We try to visit each pair of categories on average 8 times
+        batch_size = 256
+        max_datapoints = 32768 # (2^15)
+        n = (8 * (data_iterator.nb_categories**2))
+        #n = data_iterator.size(data_type='test', no_evaluation=False)
+        n = min(max_datapoints, n)
+        nb_batch = int(np.ceil(n / batch_size))
+
+        # these are not going to change, since this is not inherited by the population settings
+        sender, receiver, drawer = self.agents
+
+
+        messages = []
+        categories = []
+        batch_numbers = range(nb_batch)
+        if(self.autologger.display == 'tqdm'): batch_numbers = tqdm.tqdm(batch_numbers, desc='Eval.')
+        with torch.no_grad():
+            success = [] # Binary
+            success_prob = [] # Probabilities
+            scrambled_success_prob = [] # Probabilities
+            charlie_success = [] # Binary
+
+            for _ in batch_numbers:
+                #self.start_episode(train_episode=False) # Select agents at random if necessary
+                batch = data_iterator.get_batch(batch_size, data_type='test', no_evaluation=False, sampling_strategies=['different'], keep_category=True) # We use all categories and use only one distractor from a different category
+                # send
+                sender_outcome = sender(self._alice_input(batch))
+                # adversarial step
+                drawer_outcome = drawer(*sender_outcome.action)
+                # receive
+                bob_input = self._charlied_bob_input(batch, drawer_outcome.image)
+                receiver_outcome = receiver(bob_input, *sender_outcome.action)
+
+                receiver_pointing = pointing(receiver_outcome.scores, argmax=True)
+                success.append((receiver_pointing['action'] == 0).float())
+                success_prob.append(receiver_pointing['dist'].probs[:, 0]) # Probability of the target
+                charlie_success.append((receiver_pointing['action'] == 2).float())
+
+
+                target_category = [data_iterator.category_idx(x.category) for x in batch.original]
+                distractor_category = [data_iterator.category_idx(x.category) for base_distractors in batch.base_distractors for x in base_distractors]
+
+                failure = receiver_pointing['dist'].probs[:, 1].cpu().numpy() # Probability of the distractor
+                data_iterator.failure_based_distribution.update(target_category, distractor_category, failure)
+
+                np.add.at(counts_matrix, (target_category, distractor_category), 1.0)
+                np.add.at(failure_matrix, (target_category, distractor_category), failure)
+
+                scrambled_messages = sender_outcome.action[0].clone().detach() # We have to be careful as we probably don't want to modify the original messages
+                for i, datapoint in enumerate(batch.original): # Saves the (message, category) pairs and prepare for scrambling
+                    msg = sender_outcome.action[0][i]
+                    msg_len = sender_outcome.action[1][i]
+                    cat = datapoint.category
+
+                    if((not self.correct_only) or (receiver_pointing['action'][i] == 0)):
+                        messages.append(msg.tolist()[:msg_len])
+                        categories.append(cat)
+
+                    # Here, I am scrambling the whole message, including the EOS (but not the padding symbols, of course)
+                    l = msg_len.item()
+                    scrambled_messages[i, :l] = scrambled_messages[i][torch.randperm(l)]
+
+                scrambled_receiver_outcome = self.get_receiver()(bob_input, message=scrambled_messages, length=sender_outcome.action[1])
+                scrambled_receiver_pointing = pointing(scrambled_receiver_outcome.scores)
+                scrambled_success_prob.append(scrambled_receiver_pointing['dist'].probs[:, 0])
+
+            success_prob = torch.stack(success_prob)
+            scrambled_success_prob = torch.stack(scrambled_success_prob)
+            scrambling_resistance = (torch.stack([success_prob, scrambled_success_prob]).min(0).values.mean().item() / success_prob.mean().item()) # Between 0 and 1. We take the min in order to not count messages that become accidentaly better after scrambling
+            log('eval/scrambling-resistance', scrambling_resistance)
+
+            # Here, we try to see how much the messages describe the categories and not the praticular images
+            # To do so, we use the original image as target, and an image of the same category as distractor
+            abstractness = []
+            n = (32 * data_iterator.nb_categories)
+            n = min(max_datapoints, n)
+            nb_batch = int(np.ceil(n / batch_size))
+            for _ in range(nb_batch):
+                batch = data_iterator.get_batch(batch_size, data_type='test', no_evaluation=False, sampling_strategies=['same'], target_is_original=True, keep_category=True)
+                # send
+                sender_outcome = sender(self._alice_input(batch))
+
+                # adversarial step
+                drawer_outcome = drawer(*sender_outcome.action)
+
+                # receive
+                receiver_outcome = receiver(self._charlied_bob_input(batch, drawer_outcome.image), *sender_outcome.action)
+
+                receiver_pointing = pointing(receiver_outcome.scores)
+                abstractness.append(receiver_pointing['dist'].probs[:, 1] * 2.0)
+
+            abstractness = torch.stack(abstractness)
+            abstractness_rate = abstractness.mean().item()
+            log('eval/abstractness', abstractness_rate)
+
+        # Here we computing the actual success rate with argmax pointing, and not the mean expected success based on probabilities like is done after
+        success = torch.stack(success)
+        success_rate = success.mean().item()
+        log('eval/success_rate', success_rate)
+
+        charlie_success = torch.stack(charlie_success)
+        charlie_success_rate = charlie_success.mean().item()
+        log('eval/charlie_success_rate', charlie_success_rate)
+
+        # Computes the accuracy when the images are selected from all categories
+        accuracy_all = 1 - (failure_matrix.sum() / counts_matrix.sum())
+        log('eval/accuracy', accuracy_all)
+
+        train_categories = data_iterator.training_categories_idx
+        eval_categories = data_iterator.evaluation_categories_idx
+        if(eval_categories != []):
+            # Computes the accuracy when both the target and the distractor are selected from training categories
+            failure_matrix_train_td = failure_matrix[np.ix_(train_categories, train_categories)]
+            counts_matrix_train_td = counts_matrix[np.ix_(train_categories, train_categories)]
+
+            counts = counts_matrix_train_td.sum()
+            accuracy_train_td = (1 - (failure_matrix_train_td.sum() / counts)) if(counts > 0.0) else -1
+            log('eval/accuracy-train-td', accuracy_train_td)
+
+            # Computes the accuracy when the target is selected from an evaluation category (never seen during training)
+            failure_matrix_eval_t = failure_matrix[eval_categories, :]
+            counts_matrix_eval_t = counts_matrix[eval_categories, :]
+
+            counts = counts_matrix_eval_t.sum()
+            accuracy_eval_t = (1 - (failure_matrix_eval_t.sum() / counts)) if(counts > 0.0) else -1
+            log('eval/accuracy-eval-t', accuracy_eval_t)
+
+            # Computes the accuracy when the distractor is selected from an evaluation category (never seen during training)
+            failure_matrix_eval_d = failure_matrix[:, eval_categories]
+            counts_matrix_eval_d = counts_matrix[:, eval_categories]
+
+            counts = counts_matrix_eval_d.sum()
+            accuracy_eval_d = (1 - (failure_matrix_eval_d.sum() / counts)) if(counts > 0.0) else -1
+            log('eval/accuracy-eval-d', accuracy_eval_d)
+
+            # Computes the accuracy when both the target and the distractor are selected from evaluation categories (never seen during training)
+            failure_matrix_eval_td = failure_matrix[np.ix_(eval_categories, eval_categories)]
+            counts_matrix_eval_td = counts_matrix[np.ix_(eval_categories, eval_categories)]
+
+            counts = counts_matrix_eval_td.sum()
+            accuracy_eval_td = (1 - (failure_matrix_eval_td.sum() / counts)) if(counts > 0.0) else -1
+            log('eval/accuracy-eval-td', accuracy_eval_td)
+
+        self.switch_charlie(training_state)
