63842cae1097739cc92d8fef528051d9506b8d5c
Timothée Bernard <tot.c.moi@gmail.com>
2025-10-23 18:50:48 +0200

    property -> predicate

--- concon/predicate_signalling_game.py
+++ concon/predicate_signalling_game.py
@@ -8,7 +8,7 @@ import torch.nn as nn
 from torch.utils.tensorboard import SummaryWriter
 import tqdm
 
-from .utils.property_data import get_data_loader
+from .utils.predicate import get_data_loader
 from .utils.misc import build_optimizer, get_default_fn, path_replace
 from .utils.logging import AutoLogger
 
