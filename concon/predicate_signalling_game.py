f0b389ffbec52c74b77ca8af7548d9d6815c4d69
csogreen <mikolajgolecki@outlook.com>
2026-02-13 11:08:58 +0100

    minor grammar update

--- concon/predicate_signalling_game.py
+++ concon/predicate_signalling_game.py
@@ -101,7 +101,7 @@ def do(args):
         # If the model has not reached a certain performance threshold during training, an empty "FAILURE" file is created.
         performance_threshold = 0.6
         if(model.max_perf < performance_threshold):
-            print("This runs has failed.")
+            print("This run has failed.")
             if(not args.no_summary):
                 filename = run_summary_dir / "FAILURE"
                 open(filename, 'a').close()
