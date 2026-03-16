8aa954b766fbbf4da0b088ec28bcab08d8a9163b
csogreen <mikolajgolecki@outlook.com>
2026-03-16 20:55:10 +0100

    convenience for test

--- concon/eval/predicate/analysis.py
+++ concon/eval/predicate/analysis.py
@@ -1,9 +1,11 @@
+import os, pathlib
 from load import get_datapoints
 
 if __name__ == "__main__":
     # Ask for experimental data
     # Assumes that all runs for one experiment are written into a dedicated folder
     # And that that folder is inside "runs/"
+    print(f"Available experiments: {os.listdir(pathlib.Path('runs'))}")
     experiment_path = input("Experiment name: ")
     datapoints = get_datapoints(experiment_path)
 
